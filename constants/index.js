60bc78d3f1417d0a256ee5998c537bc1afa2b04b
mauro sala <mauro.sala@gmail.com>
2022-05-03 17:49:16 +0200

    fix(call-logger): no log paths

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,7 @@
 const envConstants = require('./env.constants')
+const pathConstants = require('./path.constants')
 
 module.exports = {
-  envConstants
+  envConstants,
+  pathConstants
 }
--- middlewares/call-logger.middleware.js
+++ middlewares/call-logger.middleware.js
@@ -1,16 +1,26 @@
 const { logger } = require('../helpers/logger.helpers')
+const { pathConstants } = require('../constants')
 
 module.exports = (req, res, next) => {
-  logger.info(`${req.path} - ${req.method} - ${req.ip}`)
+  let skip = false
+  pathConstants.noLoggingPath.forEach((r) => {
+    const base = `/${req.originalUrl.split('/')[1]}/*`
+    if (r === req.originalUrl || r === base) {
+      skip = true
+    }
+  })
 
-  if (Object.keys(req.body).length > 0) {
-    logger.debug(JSON.stringify(req.body))
-  }
-  if (Object.keys(req.params).length > 0) {
-    logger.debug(JSON.stringify(req.params))
-  }
-  if (Object.keys(req.query).length > 0) {
-    logger.debug(JSON.stringify(req.query))
+  if (!skip) {
+    logger.info(`${req.path} - ${req.method} - ${req.ip}`)
+    if (Object.keys(req.body).length > 0) {
+      logger.debug(JSON.stringify(req.body))
+    }
+    if (Object.keys(req.params).length > 0) {
+      logger.debug(JSON.stringify(req.params))
+    }
+    if (Object.keys(req.query).length > 0) {
+      logger.debug(JSON.stringify(req.query))
+    }
   }
 
   next()
